a33b39788ee16fb51db8d290f60b7e47db29ef2e
lsmucassi <lindasmucassi@gmail.com>
2020-06-02 13:38:51 +0200

    get n display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { fetchWeather } from './api/fetchWeather'
+import './App.css'
 
 const App = () => {
+    const [query, setQuery] = useState('')
+    const  [weather, setWeather] = useState({})
+
+    const search = async (e) => {
+        if (e.key === 'Enter') {
+            const data = await fetchWeather(query)
+
+            setWeather(data)
+            setQuery('')
+        }
+    }
+
     return (
-        <h1>App</h1>
+        <div className="main-container">
+            <input
+                type="text"
+                className="search"
+                placeholder="search city"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)} />
+                {weather.main && (
+                    <div className="city">
+                        <h2 className="city-name">
+                            <span>{weather.name}</span>
+                            <span>{weather.sys.country}</span>
+                        </h2>
+                        <div className="city-temp">
+                            
+                        </div>
+                    </div>
+                    
+                )}
+
+        </div>
     )
 }
 
