dee11b4af478f7c8279248cb7bdcf7fb8078eade
lsmucassi <lindasmucassi@gmail.com>
2020-06-02 15:02:27 +0200

    pwa cache handlling

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -17,10 +17,28 @@ self.addEventListener('install', (event) => {
 
 //Listen for requests
 self.addEventListener('fetch', (event) => {
-
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch((err) => caches.match('off-line.html'))
+            })
+    )
 })
 
 // activate the SW
 self.addEventListener('activate', (event) => {
+    const cacheWhitelist = []
+    cacheWhitelist.push(CACHE_NAME)
 
-})
+    event.waitUntil(
+        caches.keys().then((cacheNames) = Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName)
+                }
+            })
+    )
+
+}
+)
